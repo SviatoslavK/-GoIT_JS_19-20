06bcf2ba407b76d4a3e4f69997b53084249e8fe5
SviatoslavK <kolodaslav@gmail.com>
2016-09-24 23:51:40 +0300

    Added sprite task, corrected  paths files

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,42 @@
 'use strict';
 
-var gulp = require('gulp'),
-    watch = require('gulp-watch'),
-    uglify = require('gulp-uglify'),
-    sass = require('gulp-sass'),
-    rigger = require('gulp-rigger'),
-    cleanCSS = require('gulp-clean-css'),
-    imagemin = require('gulp-imagemin'),
-    pngquant = require('imagemin-pngquant'),
-    rimraf = require('rimraf');
+var gulp        = require('gulp'),
+    watch       = require('gulp-watch'),
+    uglify      = require('gulp-uglify'),
+    sass        = require('gulp-sass'),
+    rigger      = require('gulp-rigger'),
+    cleanCSS    = require('gulp-clean-css'),
+    imagemin    = require('gulp-imagemin'),
+    pngquant    = require('imagemin-pngquant'),
+    cache       = require('gulp-cache'),
+    spritesmith = require('gulp.spritesmith'),
+    rimraf      = require('rimraf');
 
 var path = {
     build: {
         html: 'build/',
         js: 'build/js',
-        css: 'build/css/main',
+        css: 'build/css',
         img: 'build/img',
-        fonts: 'buils/fonts'
+        fonts: 'build/fonts',
+        spritesImg: 'build/img/sprite',
+        spritesSCSS: 'src/style/partials'
     },
     src: {
         html: 'src/*.html',
         js: 'src/js/main.js',
         style: 'src/style/main.scss',
-        img: 'src/img/**/*.*',
-        fonts: 'src/fonts/**/*.*'
+        img: 'src/style/img/**/*.*',
+        fonts: 'src/fonts/**/*.*',
+        sprites: 'src/style/sprite/*.*'
     },
     watch: {
         html: 'src/**/*.html',
         js: 'src/js/**/*.js',
         style: 'src/style/**/*.scss',
-        img: 'src/img/**/*.*',
-        fonts: 'src/fonts/**/*.*'
+        img: 'src/style/img/**/*.*',
+        fonts: 'src/fonts/**/*.*',
+        sprites: 'src/style/sprite/*.*'
     },
     clean: './build'
 };
@@ -57,13 +63,26 @@ gulp.task('style:build', function() {
 
 gulp.task('image:build', function(){
     gulp.src(path.src.img)
-        .pipe(imagemin({
+        .pipe(cache(imagemin({
             progressive: true,
             svgoPlugins: [{removeViewBox: false}],
             use: [pngquant()],
             interlaced: true
-    }))
+    })))
         .pipe(gulp.dest(path.build.img));
+});
+
+gulp.task('sprite:build', function() {
+    var spriteData =
+        gulp.src(path.src.sprites)
+    .pipe(spritesmith({
+        imgName: 'sprite.png',
+        cssName: 'sprite.scss',
+        imgPath: '../img/sprite/sprite.png',
+    }));
+
+    spriteData.img.pipe(gulp.dest(path.build.spritesImg)); // путь, куда сохраняем картинку
+    spriteData.css.pipe(gulp.dest(path.build.spritesSCSS)); // путь, куда сохраняем стили
 });
 
 gulp.task('fonts:build', function() {
@@ -95,10 +114,17 @@ gulp.task('watch', function() {
     watch([path.watch.fonts], function(event, cb) {
         gulp.start('fonts:build');
     });
+    watch([path.watch.sprites], function(event, cb) {
+        gulp.start('sprite:build');
+    });
 });
 
 gulp.task('clean', function(cb) {
     rimraf(path.clean, cb);
 });
 
-gulp.task('default', ['build', 'watch']);
+gulp.task('clear', function() {
+    return cache.clearAll();
+});
+
+gulp.task('default', [ 'build', 'watch']);
